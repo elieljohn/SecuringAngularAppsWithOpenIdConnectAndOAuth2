d1f3b7af043682f2764e5ad57fb2d1a1237f13ba
elieljohn <ejmalabar88@gmail.com>
2024-06-24 13:55:07 +0100

    Configuring oidc-client to Connect to Your STS
    1. Modify AuthService, which is handles user authentication and authorization. It uses the oidc-client library to interact with an OpenID Connect (OIDC) provider, which is a server that handles user authentication and provides identity information.
    auth-service.component.ts

--- GlobomanticsProject/src/app/core/auth-service.component.ts
+++ GlobomanticsProject/src/app/core/auth-service.component.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
-import { CoreModule } from './core.module';
+import { Injectable } from "@angular/core";
+import { CoreModule } from "./core.module";
+import { User, UserManager } from "oidc-client";
+import { Constants } from "../constants";
 
-@Injectable({providedIn: CoreModule})
+
+@Injectable({ providedIn: CoreModule })
 export class AuthService {
-    
-    constructor() { }
-    
+  private _userManager: UserManager;
+  private _user: User;
+
+  constructor() {
+    const stsSettings = {
+      authority: Constants.stsAuthority,
+      client_id: Constants.clientId,
+      redirect_uri: `${Constants.clientRoot}signin-callback`,
+      scope: 'openid profile projects-api',
+      response_type: 'code',
+      post_logout_redirect_uri: `${Constants.clientRoot}signout-callback`
+    };
+    // Configure the UserManager using the settings in stsSettings
+    // Initialize _userManager in the constructor
+    this._userManager = new UserManager(stsSettings);
+  }
 }
